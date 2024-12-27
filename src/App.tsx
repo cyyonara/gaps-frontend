234bd937055b5eee45810e9cd0106b618dccbb49
Cyrel Villanueva <villanuevacyrel30@gmail.com>
2024-12-27 22:27:20 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button';
+
+const App = () => {
+  return (
+    <div className='text-red-500'>
+      <Button>G na Baks</Button>
+    </div>
+  );
+};
+
+export default App;
