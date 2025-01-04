04ea423720bcce3c841695f9fc6b51cdf94c5870
Marinelle Ando <justmarinelle05@gmail.com>
2025-01-04 20:05:33 +0800

    login validations

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -6,10 +6,32 @@ import { FaEnvelope, FaLock } from "react-icons/fa";
 import { Button } from "@/components/ui/button";
 import { FaEye } from "react-icons/fa";
 import { FaEyeSlash } from "react-icons/fa";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { ILoginFields } from "@/types";
+import { loginSchema } from "@/helpers/validations";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm, SubmitHandler } from "react-hook-form";
 
 const LoginPage = () => {
   const [showPassword, setShowPassword] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    setFocus,
+    formState: { errors },
+  } = useForm<ILoginFields>({
+    resolver: zodResolver(loginSchema),
+  });
+
+  const handleLogin: SubmitHandler<ILoginFields> = (formData) => {
+    console.log(JSON.stringify(formData));
+  };
+
+  useEffect(() => {
+    setFocus("email");
+  }, [setFocus]);
+
   return (
     <div
       className="w-full h-[100vh] bg-background flex flex-col justify-center items-center bg-cover bg-center bg-white"
@@ -26,46 +48,61 @@ const LoginPage = () => {
         </CardHeader>
 
         <CardContent>
-          <form action="">
-            <div className="relative w-full mb-4">
-              <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                <FaEnvelope />
+          <form id="loginForm" onSubmit={handleSubmit(handleLogin)}>
+            <div className="flex flex-col space-y-4 items-start">
+              <div>
+                <div className="relative flex-1 flex items-center pl-7 border border-black rounded-md focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-primary">
+                  <div className="absolute inset-y-0 left-0 px-3 flex items-center pointer-events-none">
+                    <FaEnvelope />
+                  </div>
+                  <Input
+                    type="email"
+                    {...register("email")}
+                    placeholder="Enter Email Address"
+                    className=" placeholder:text-black ring-0 focus-visible:ring-0 focus-visible:border-0 focus-visible:outline-none shadow-none inset-0 border-none"
+                  />
+                </div>
+                {errors.email && <p className="text-primary text-xs">{errors.email.message}</p>}
               </div>
-              <Input
-                type="email"
-                placeholder="Enter Email Address"
-                className="pl-10 pr-3 py-2 border border-black rounded-md w-full focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-primary focus-visible:border-transparent placeholder:text-black"
-              />
-            </div>
-            <div className="relative w-full">
-              <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                <FaLock />
-              </div>
-              <Input
-                type={showPassword ? "text" : "password"}
-                placeholder="Enter Password"
-                className="pl-10 pr-3 py-2 border border-black rounded-md w-full focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-primary focus-visible:border-transparent placeholder:text-black"
-              />
-              <div className="absolute inset-y-0 right-0 pr-3 flex items-center">
-                {showPassword ? (
-                  <button type="button">
-                    <FaEye
-                      className="text-black cursor-pointer"
-                      onClick={() => setShowPassword(!showPassword)}
-                    />
-                  </button>
-                ) : (
-                  <button type="button">
-                    <FaEyeSlash
-                      className="text-black cursor-pointer"
-                      onClick={() => setShowPassword(!showPassword)}
-                    />
-                  </button>
+              <div>
+                <div className="relative flex items-start flex-col pl-7 border border-black rounded-md flex-1 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-primary focus-visible:border-transparent">
+                  <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                    <FaLock />
+                  </div>
+                  <Input
+                    {...register("password")}
+                    type={showPassword ? "text" : "password"}
+                    placeholder="Enter Password"
+                    className=" placeholder:text-black border-none ring-0 focus-visible:ring-0 focus-visible:border-0 focus-visible:outline-none shadow-none inset-0"
+                  />
+
+                  <div className="absolute inset-y-0 right-0 pr-3 flex items-center">
+                    {showPassword ? (
+                      <button type="button">
+                        <FaEye
+                          className="text-black cursor-pointer"
+                          onClick={() => setShowPassword(!showPassword)}
+                        />
+                      </button>
+                    ) : (
+                      <button type="button">
+                        <FaEyeSlash
+                          className="text-black cursor-pointer"
+                          onClick={() => setShowPassword(!showPassword)}
+                        />
+                      </button>
+                    )}
+                  </div>
+                </div>
+                {errors.password && (
+                  <p className="text-primary text-xs">{errors.password.message}</p>
                 )}
               </div>
             </div>
           </form>
-          <Button className="w-full mt-4">Login</Button>
+          <Button type="submit" form="loginForm" className="w-full mt-4">
+            Login
+          </Button>
         </CardContent>
       </Card>
     </div>
